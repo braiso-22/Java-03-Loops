b144a19c56da7d658e494fefc197734d7eab6b67
Brais <81094589+braiso-22@users.noreply.github.com>
2021-12-22 14:11:30 +0100

    Ej06 acabado

---
+++ Java-03-Loops/src/loops/Ej06.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loops;
+
+/**
+ *
+ * @author Brais
+ */
+public class Ej06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 100; i > 0; i -= 2) {
+            System.out.println(i);
+        }
+    }
+}
