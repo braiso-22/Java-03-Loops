786c1498f9692f4143cf0ae8d1005b442e701659
Brais <81094589+braiso-22@users.noreply.github.com>
2021-12-23 19:04:42 +0100

    Ej27c terminado

---
+++ Java-03-Loops/src/loops/Ej27/c.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loops.Ej27;
+
+/**
+ *
+ * @author Brais
+ */
+public class c {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 9; j >= i; j--) {
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
+    }
+
+}
